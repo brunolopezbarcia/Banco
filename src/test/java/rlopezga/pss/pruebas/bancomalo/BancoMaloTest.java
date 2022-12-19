f477dbe80eae69645a95dad84db11c130332647d
Bruno López Barcia <brunolopar46@gmail.com>
2022-12-19 23:02:08 +0100

    Añadidos los test de BancoMalo

---
+++ src/test/java/rlopezga/pss/pruebas/bancomalo/BancoMaloTest.java
@@ -0,0 +1,39 @@
+package rlopezga.pss.pruebas.bancomalo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BancoMaloTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void getNombreBanco() {
+    }
+
+    @Test
+    void getCuentas() {
+    }
+
+    @Test
+    void getCuenta() {
+    }
+
+    @Test
+    void agregarCuenta() {
+    }
+
+    @Test
+    void quitarCuenta() {
+    }
+}
